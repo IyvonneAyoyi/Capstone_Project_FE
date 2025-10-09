9f2d22d30028b98c9ebc648e9a064f3a4aabf20d
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-09 22:20:05 +0300

    Fetch rainfall and risk levels for all towns

--- kenya-precip-monitor-app/src/components/MapComponent.jsx
+++ kenya-precip-monitor-app/src/components/MapComponent.jsx
@@ -6,4 +6,17 @@ import {useState } from "react";
 const MapComponent = () => {
   const [townData, setTownData] = useState([]);
 
-  
+    useEffect(() => {
+    async function fetchData() {
+      const results = await Promise.all(
+        towns.map(async (town) => {
+          const rainfall = await getDailyRainfall(town.Latitude, town.Longitude);
+          const risk = getRiskLevel(rainfall);
+          return { ...town, rainfall, risk };
+        })
+      );
+      setTownData(results);
+    }
+
+    fetchData();
+  }, []);
