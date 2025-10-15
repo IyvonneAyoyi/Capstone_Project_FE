0214a2014e7b498a5cc781718dcf145e1b15a171
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-15 14:23:25 +0300

    Add support for past_days and forecast_days in weatherAPI

--- kenya-precip-monitor-app/src/api/weatherAPI.js
+++ kenya-precip-monitor-app/src/api/weatherAPI.js
@@ -1,9 +1,17 @@
-export async function getDailyRainfall(lat, lon) {
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=precipitation_sum&timezone=Africa%2FNairobi`;
+export async function getDailyRainfall(lat, lon, option = { type: "forecast", days: 1 }) {
+  let param =
+    option.type === "past"
+      ? `past_days=${option.days}`
+      : `forecast_days=${option.days}`;
 
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=precipitation_sum&timezone=Africa%2FNairobi&${param}`;
+
+  
   try {
     const res = await fetch(url);
     const data = await res.json();
+
+    // Return average rainfall or first value
     return data?.daily?.precipitation_sum?.[0] ?? null;
   } catch (error) {
     console.error("Error fetching rainfall data:", error);
