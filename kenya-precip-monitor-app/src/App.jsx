0e4f5faaa40038221af88564d5e3a51be2afd2cc
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-18 01:25:44 +0300

    Feat:Implement React Router for navigation between Home and About pages

--- kenya-precip-monitor-app/src/App.jsx
+++ kenya-precip-monitor-app/src/App.jsx
@@ -1,12 +1,25 @@
-// App.jsx
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
