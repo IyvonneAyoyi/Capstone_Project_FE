a3c97c289eb918ef933c57bbfa656d4ce64b41a2
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-17 19:52:22 +0300

    Minor whitespace adjustment for className in Navbar component

--- kenya-precip-monitor-app/src/components/Navbar.jsx
+++ kenya-precip-monitor-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-    <h1 className="font-bold text-lg tracking-wide">PrecipMonitor KE</h1>
+    <h1 className="font-bold text-lg tracking-wide ">PrecipMonitor KE</h1>
     <div className="flex gap-4 text-sm sm:text-base">
       <Link to="/" className="hover:underline">Home</Link>
       <Link to="/about" className="hover:underline">About</Link>
